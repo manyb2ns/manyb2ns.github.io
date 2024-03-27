3de251723b663da98428b892f64774da470294ba
manyb2ns <manyb2ns@gmail.com>
2024-03-27 15:10:55 +0900

    test: n2m.toMarkdownString(mdblocks) 출력

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -103,12 +103,14 @@ title: "${title}"${fmtags}${fmcats}
     const mdblocks = await n2m.pageToMarkdown(id);
 
     console.log("n2m: ",n2m)  // n2m_value 파일 참고
-    console.log("n2m pages: ",n2m["pages"])
+    console.log("n2m pages: ",n2m["pages"]) // undefined
     console.log('n2m.pageToMarkdown(id): ',n2m.pageToMarkdown(id))  // [object Promise] --->  Promise { <pending> }
     console.log('n2m.toMarkdownString(mdblocks)["parent"]: ',n2m.toMarkdownString(mdblocks)["parent"])  // undefined
     console.log("mdblocks: ",mdblocks)  // []
+    console.log("n2m.toMarkdownString(mdblocks)",n2m.toMarkdownString(mdblocks))
 
-    let md = n2m.toMarkdownString(mdblocks)["parent"];  // undefined
+    // let md = n2m.toMarkdownString(mdblocks)["parent"];  // undefined
+    let md = n2m.toMarkdownString(mdblocks)["pages"];
     md = escapeCodeBlock(md);
 
     const ftitle = `${date}-${title.replaceAll(" ", "-")}.md`;
